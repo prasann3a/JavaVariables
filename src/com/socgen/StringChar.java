86db0b386fc2fb7a5a318e64e92a3db5b2ab0220
prasann3a <prasanna6796@gmail.com>
2022-03-16 09:18:35 +0530

    String Char

---
+++ src/com/socgen/StringChar.java
@@ -0,0 +1,14 @@
+package com.socgen;
+
+public class StringChar {
+    public static void main(String[] args) {
+        char myChar='D';
+        char myUnicodeChar='\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar='\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanValue= true;
+        boolean myFalseBooleanValue= false;
+    }
+}
