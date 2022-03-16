1539e68e3ee86b0c7300848467998d38946533a3
prasann3a <prasanna6796@gmail.com>
2022-03-16 07:31:21 +0530

    first code on variables

--- src/com/socgen/Main.java
+++ src/com/socgen/Main.java
@@ -1,14 +1,14 @@
 package com.socgen;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//	// write your code here
-//        int myValue=1000;
-//        int myMinIntValue= Integer.MIN_VALUE;
-//        int myMaxIntValue= Integer.MAX_VALUE;
-//        System.out.println(myMaxIntValue);
-//        System.out.println(myMinIntValue);
-//    }
-//}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myValue=1000;
+        int myMinIntValue= Integer.MIN_VALUE;
+        int myMaxIntValue= Integer.MAX_VALUE;
+        System.out.println(myMaxIntValue);
+        System.out.println(myMinIntValue);
+    }
+}
 //// overflow and underflow of integer
