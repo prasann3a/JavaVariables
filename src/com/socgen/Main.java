cdbaffebfb7f4d2a75e086045b0f52828f38018e
prasann3a <prasanna6796@gmail.com>
2022-03-16 09:15:02 +0530

    new codes

--- src/com/socgen/Main.java
+++ src/com/socgen/Main.java
@@ -9,6 +9,20 @@ public class Main {
         int myMaxIntValue= Integer.MAX_VALUE;
         System.out.println(myMaxIntValue);
         System.out.println(myMinIntValue);
+
+        byte myMinByteValue=Byte.MIN_VALUE;
+        byte myMaxByteValue=Byte.MAX_VALUE;
+        System.out.println("Integer Minimum Value= "+ myMinByteValue);
+        System.out.println("Integer Maximum Value= "+ myMinByteValue);
+
+        long myMinLongValue=Long.MIN_VALUE;
+        long myMaxLongValue=Long.MAX_VALUE;
+        System.out.println("Integer Minimum Value= "+ myMinLongValue);
+        System.out.println("Integer Maximum Value= "+ myMinLongValue);
+
+        int myTotal= (myMinIntValue/2);
+        byte myNewByteValue=(byte)(myMinByteValue/(2));
+
     }
 }
 //// overflow and underflow of integer
